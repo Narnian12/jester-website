6fed3f1c049cf0f9972d45153c2b72ce3156da24
Crystal Sun <crysun@umich.edu>
2024-07-18 20:28:32 -0500

    Create password page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,32 +1,41 @@
 import Home from '../views/Home.vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import Password from '../views/Password.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-const history = createWebHashHistory()
+// Define routes
 const routes = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: Password
+  },
   {
     path: '/',
     name: 'Home',
     component: Home,
-    meta: {
-      layout: 'default'
-    }
+    meta: { requiresAuth: true, layout: 'Home' } // This route requires authentication
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-    meta: {
-      layout: 'default'
-    }
+    path: '/:catchAll(.*)',
+    redirect: '/login' // Redirect all unknown routes to /login
   }
 ]
+
+// Create router instance
 const router = createRouter({
-  linkActiveClass: 'active',
-  history,
+  history: createWebHistory(),
   routes
 })
 
-export { router }
+// Navigation guard for authentication
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = !!localStorage.getItem('auth') // Check if authenticated
+
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next('/login') // Redirect to login if not authenticated
+  } else {
+    next() // Proceed to the route
+  }
+})
+
+export default router
